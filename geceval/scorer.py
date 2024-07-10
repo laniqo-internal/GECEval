e05c219dbcd56ea1a0d56b9a6092279c9d97c294
Dawid Wisniewski <dawid.wisniewski@laniqo.com>
2024-07-10 07:43:20 +0200

    Refactor

--- geceval/scorer.py
+++ geceval/scorer.py
@@ -1,9 +1,11 @@
 import os
 from enum import Enum
 from typing import List, Optional
-from geceval.triton_communication import read_triton_output, read_triton_request
-from geceval.modules.spell_checker_module import SpellcheckerModule
+
 from geceval.modules.language_tool_module import LanguageToolModule
+from geceval.modules.spell_checker_module import SpellcheckerModule
+from geceval.triton_communication import (read_triton_output,
+                                          read_triton_request)
 
 
 class GECModules(Enum):
@@ -18,11 +20,13 @@ class GECModules(Enum):
 
 
 class Scorer:
-    def __init__(self, supported_modules: Optional[List[GECModules]], languages: List[str]):
+    def __init__(
+        self, supported_modules: Optional[List[GECModules]], languages: List[str]
+    ):
         self.supported_modules = supported_modules
         self.languages = languages
         self.evaluators = self.construct_evaluators(supported_modules, languages)
-    
+
     def construct_evaluators(self, supported_modules, languages):
         evaluators = {}
         supported_modules = set(supported_modules)
@@ -41,22 +45,26 @@ class Scorer:
             language_path = os.path.join(path, language)
 
             for idx in range(10000):
-                correct_path = os.path.join(language_path, f'correct_{idx}.json')
-                incorrect_path = os.path.join(language_path, f'incorrect_{idx}.json')
+                correct_path = os.path.join(language_path, f"correct_{idx}.json")
+                incorrect_path = os.path.join(language_path, f"incorrect_{idx}.json")
                 if os.path.exists(correct_path):
-                    result[language].append({
-                        "idx": idx,
-                        "marked_correct": 1,
-                        "before_correction": read_triton_request(correct_path),
-                        "after_correction": read_triton_output(correct_path)
-                    })
+                    result[language].append(
+                        {
+                            "idx": idx,
+                            "marked_correct": 1,
+                            "before_correction": read_triton_request(correct_path),
+                            "after_correction": read_triton_output(correct_path),
+                        }
+                    )
                 elif os.path.exists(incorrect_path):
-                    result[language].append({
-                        "idx": idx,
-                        "marked_correct": 0,
-                        "before_correction": read_triton_request(incorrect_path),
-                        "after_correction": read_triton_output(incorrect_path)
-                    })
+                    result[language].append(
+                        {
+                            "idx": idx,
+                            "marked_correct": 0,
+                            "before_correction": read_triton_request(incorrect_path),
+                            "after_correction": read_triton_output(incorrect_path),
+                        }
+                    )
                 else:
                     continue
         return result
@@ -68,24 +76,32 @@ class Scorer:
                 "idx": [],
                 "marked_correct": [],
                 "before_correction": [],
-                "after_correction": []
+                "after_correction": [],
             }
             language_path = os.path.join(path, language)
 
             for idx in range(10000):
-                correct_path = os.path.join(language_path, f'correct_{idx}.json')
-                incorrect_path = os.path.join(language_path, f'incorrect_{idx}.json')
+                correct_path = os.path.join(language_path, f"correct_{idx}.json")
+                incorrect_path = os.path.join(language_path, f"incorrect_{idx}.json")
                 if os.path.exists(correct_path):
-                    result[language]['idx'].append(idx)
-                    result[language]['marked_correct'].append(1)
-                    result[language]['before_correction'].append(read_triton_request(correct_path))
-                    result[language]['after_correction'].append(read_triton_output(correct_path))
+                    result[language]["idx"].append(idx)
+                    result[language]["marked_correct"].append(1)
+                    result[language]["before_correction"].append(
+                        read_triton_request(correct_path)
+                    )
+                    result[language]["after_correction"].append(
+                        read_triton_output(correct_path)
+                    )
 
                 elif os.path.exists(incorrect_path):
-                    result[language]['idx'].append(idx)
-                    result[language]['marked_correct'].append(0)
-                    result[language]['before_correction'].append(read_triton_request(incorrect_path))
-                    result[language]['after_correction'].append(read_triton_output(incorrect_path))
+                    result[language]["idx"].append(idx)
+                    result[language]["marked_correct"].append(0)
+                    result[language]["before_correction"].append(
+                        read_triton_request(incorrect_path)
+                    )
+                    result[language]["after_correction"].append(
+                        read_triton_output(incorrect_path)
+                    )
                 else:
                     continue
         return result
@@ -94,14 +110,18 @@ class Scorer:
         data = self.load_data_to_dict_of_lists(path)
         for language in self.languages:
             for module in self.supported_modules:
-                before_ave = self.evaluators[module].get_average_score(data[language]['before_correction'])
-                after_ave = self.evaluators[module].get_average_score(data[language]['after_correction'])
+                before_ave = self.evaluators[module].get_average_score(
+                    data[language]["before_correction"]
+                )
+                after_ave = self.evaluators[module].get_average_score(
+                    data[language]["after_correction"]
+                )
                 print(self.evaluators[module].get_name(), before_ave, after_ave)
-                #for text in data[language]['after_correction']:
+                # for text in data[language]['after_correction']:
                 #    has_error, text = self.evaluators[module].explain_errors(text)
                 #    if has_error:
                 #        print(text)
-    
+
     def close(self):
         for module in self.supported_modules:
             self.evaluators[module].close()
@@ -115,6 +135,6 @@ if __name__ == "__main__":
         GECModules.LANGUAGE_TOOL,
     ]
 
-    scorer = Scorer(supported_modules=modules, languages=['it'])
+    scorer = Scorer(supported_modules=modules, languages=["it"])
     scorer.evaluate_test_set(test_path)
     scorer.close()
--- geceval/triton_communication.py
+++ geceval/triton_communication.py
@@ -1,9 +1,11 @@
 import json
 
+
 def read_triton_request(path: str):
-    with open(path, 'r') as f:
-        return json.load(f)['inputs'][0]['data'][0]
+    with open(path, "r") as f:
+        return json.load(f)["inputs"][0]["data"][0]
+
 
 def read_triton_output(path: str):
-    with open(f"{path[:-5]}_inference.json", 'r') as f:
-        return json.load(f)['outputs'][0]['data'][0]
+    with open(f"{path[:-5]}_inference.json", "r") as f:
+        return json.load(f)["outputs"][0]["data"][0]
