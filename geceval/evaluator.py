8099d4d6657abe06eb15c1cf1611079cd3a6ceb4
Dawid Wisniewski <dawid.wisniewski@laniqo.com>
2024-07-30 15:15:43 +0200

    Parsing all languages at once, referenceless metrics calculated

--- geceval/evaluator.py
+++ geceval/evaluator.py
@@ -1,12 +1,7 @@
 import argparse
 import json
 from enum import Enum
-from typing import List, Optional
 
-from geceval.file_loaders import (
-    load_files_to_dict_of_lists,
-    load_files_to_list_of_dicts,
-)
 from geceval.modules.language_tool_module import LanguageToolModule
 from geceval.modules.punctuation_seeker import PunctuationSeekerModule
 from geceval.modules.spell_checker_module import SpellcheckerModule
@@ -25,106 +20,140 @@ class GECModules(Enum):
 
 
 class Evaluator:
-    def __init__(
-        self, supported_modules: Optional[List[GECModules]], languages: List[str]
-    ):
-        assert (
-            len(languages) == 1
-        ), "Currently only one language at once can be processed"
-        self.supported_modules = supported_modules
-        self.languages = languages
-        self.evaluators = self.construct_evaluators(supported_modules, languages)
+    def __init__(self):
+        self.supported_languages = ["en", "cs", "sv", "de", "it"]
 
-    def construct_evaluators(self, supported_modules, languages):
+        referenceless_modules = {
+            GECModules.LANGUAGE_TOOL,
+            GECModules.PUNCTUATION_SEEKER,
+            GECModules.SPELLCHECKING,
+        }
+
+        self.per_language_modules = {
+            lang: referenceless_modules for lang in self.supported_languages
+        }
+
+        if "cs" in self.supported_languages:
+            self.per_language_modules["cs"] = self.per_language_modules["cs"] - {
+                GECModules.LANGUAGE_TOOL,
+                GECModules.SPELLCHECKING,
+            }
+        if "sv" in self.supported_languages:
+            self.per_language_modules["sv"] = self.per_language_modules["sv"] - {
+                GECModules.SPELLCHECKING
+            }
+
+        self.evaluators = self._construct_evaluators()
+
+    def _construct_evaluators(self):
         evaluators = {}
-        supported_modules = set(supported_modules)
-        language = languages[0]
 
-        if GECModules.SPELLCHECKING in supported_modules:
-            evaluators[GECModules.SPELLCHECKING] = SpellcheckerModule(language)
-        if GECModules.LANGUAGE_TOOL in supported_modules:
-            evaluators[GECModules.LANGUAGE_TOOL] = LanguageToolModule(language)
-        if GECModules.PUNCTUATION_SEEKER in supported_modules:
-            evaluators[GECModules.PUNCTUATION_SEEKER] = PunctuationSeekerModule(
-                language
-            )
+        for language in self.supported_languages:
+            print(f"Constructing evaluators for {language}...")
+            evaluators[language] = {}
+
+            if GECModules.SPELLCHECKING in self.per_language_modules[language]:
+                evaluators[language][GECModules.SPELLCHECKING] = SpellcheckerModule(
+                    language
+                )
+            if GECModules.LANGUAGE_TOOL in self.per_language_modules[language]:
+                evaluators[language][GECModules.LANGUAGE_TOOL] = LanguageToolModule(
+                    language
+                )
+            if GECModules.PUNCTUATION_SEEKER in self.per_language_modules[language]:
+                evaluators[language][
+                    GECModules.PUNCTUATION_SEEKER
+                ] = PunctuationSeekerModule(language)
+
         return evaluators
 
-    def check_test_set(self, path: str, use_triton: bool = True):
-        data = load_files_to_dict_of_lists(
-            path,
-            use_triton_input=use_triton,
-            use_triton_output=use_triton,
-            languages=self.languages,
-        )
-        data_list_of_dicts = load_files_to_list_of_dicts(
-            path,
-            use_triton_input=use_triton,
-            use_triton_output=use_triton,
-            languages=self.languages,
-        )
-        language = self.languages[0]
+    def collect_original_texts(self, lang_data):
+        return [v["text"] for _, v in lang_data.items()]
 
-        for language in self.languages:
-            for module in self.supported_modules:
-                before_ave = self.evaluators[module].get_average_score(
-                    data[language]["before_correction"]
-                )
-                after_ave = self.evaluators[module].get_average_score(
-                    data[language]["after_correction"]
-                )
-                print(self.evaluators[module].get_name(), before_ave, after_ave)
+    def collect_corrected_texts(self, lang_data, prompt_id, model_name):
+        result = []
 
-        for language_data in data_list_of_dicts:
-            for elem in data_list_of_dicts[language_data]:
-                elem["feedback"] = {}
-                for module in self.supported_modules:
-                    label, explanation = self.evaluators[module].explain_errors(
-                        elem["after__correction"]
+        for _, entry in lang_data.items():
+            for correction in entry["corrections"]:
+                if (
+                    correction["prompt_id"] == prompt_id
+                    and correction["model_name"] == model_name
+                ):
+                    result.append(correction["content"])
+        return result
+
+    def get_prompt_ids(self, data):
+        prompt_ids = set()
+
+        for _, lang_obj in data.items():
+            for _, entry in lang_obj.items():
+                for correction in entry["corrections"]:
+                    prompt_ids.add(correction["prompt_id"])
+        return prompt_ids
+
+    def get_model_names(self, data):
+        model_names = set()
+
+        for _, lang_obj in data.items():
+            for _, entry in lang_obj.items():
+                for correction in entry["corrections"]:
+                    model_names.add(correction["model_name"])
+        return model_names
+
+    def evaluate(self, data_path: str):
+        data = {}
+        with open(data_path, "r") as f:
+            data = json.loads(f.read())
+
+        self.prompt_ids = self.get_prompt_ids(data)
+        self.model_names = self.get_model_names(data)
+
+        for language in self.supported_languages:
+            original_texts = self.collect_original_texts(data[language])
+
+            for module in self.per_language_modules[language]:
+                original_ave = self.evaluators[language][module].get_average_score(
+                    original_texts
+                )
+
+            print("-" * 80)
+            for prompt_id in self.prompt_ids:
+                for model_name in self.model_names:
+                    corrected_texts = self.collect_corrected_texts(
+                        data[language], prompt_id=prompt_id, model_name=model_name
                     )
-                    if label is False:
-                        explanation = "no errors found"
-                    elem["feedback"][self.evaluators[module].get_name()] = explanation
-                # for text in data[language]['after_correction']:
-                #    has_error, text = self.evaluators[module].explain_errors(text)
-                #    if has_error:
-                #        print(text)
+                    corrected_ave = self.evaluators[language][module].get_average_score(
+                        corrected_texts
+                    )
+                    print(
+                        f"Language: {language}\t Model: {model_name}\t prompt: {prompt_id}\t metric: {self.evaluators[language][module].get_name()}\t score: {original_ave}->{corrected_ave}"
+                    )
 
-        with open(f"{language}_raw_output.json", "w") as out:
-            out.write(json.dumps(data_list_of_dicts, indent=4, ensure_ascii=False))
+    def evaluate_referenceless(self, data_path: str):
+        pass
+
+    def evaluate_with_references(self):
+        pass
 
     def close(self):
-        for module in self.supported_modules:
-            self.evaluators[module].close()
+        for language in self.supported_languages:
+            print(f"Closing evaluators for {language}...")
+            for module in self.evaluators[language].keys():
+                self.evaluators[language][module].close()
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "-p", "--test-path", help="Base folder for data.", default="./data"
-    )
-    parser.add_argument("-l", "--language", help="Language to process.", default="en")
-    parser.add_argument(
-        "-t",
-        "--use-triton-data",
-        help="Use output serialized in Triton format.",
-        action="store_true",
+        "-e",
+        "--experiment_output_path",
+        help="JSON data.",
+        default="./data/merged_multillm.json",
     )
     args = parser.parse_args()
+    experiment_path = args.experiment_output_path
 
-    test_path = args.test_path
-    language = args.language
-    use_triton = args.use_triton_data
-
-    modules = [
-        GECModules.PUNCTUATION_SEEKER,
-    ]
-    if args.language != "cs":
-        modules.append(GECModules.LANGUAGE_TOOL)
-
-    if args.language not in ["cs", "sv"]:
-        modules.append(GECModules.SPELLCHECKING)
-
-    evaluator = Evaluator(supported_modules=modules, languages=[args.language])
-    evaluator.check_test_set(args.test_path, use_triton=args.use_triton_data)
+    evaluator = Evaluator()
+    # evaluator.check_test_set(args.test_path, use_triton=args.use_triton_data)
+    evaluator.evaluate(experiment_path)
     evaluator.close()
--- geceval/file_loaders.py
+++ geceval/file_loaders.py
@@ -1,89 +1,11 @@
 import json
-import os
 from pathlib import Path
-from typing import Dict, List
+from typing import Dict
 
 
-def get_file_path(base_path, idx, use_triton=True, input=True, language="en"):
-    prefix = "triton" if use_triton else "raw"
-    extension = "json" if use_triton else "txt"
-    base_path = os.path.join(base_path, prefix, language)
-
-    inference = "_inference" if not input else ""
-
-    correct_path = os.path.join(base_path, f"correct_{idx}{inference}.{extension}")
-    incorrect_path = os.path.join(base_path, f"incorrect_{idx}{inference}.{extension}")
-
-    if os.path.exists(correct_path):
-        return True, correct_path
-    elif os.path.exists(incorrect_path):
-        return False, incorrect_path
-
-    return False, ""
-
-
-def load_files_to_list_of_dicts(
-    path, languages=["en"], use_triton_input=True, use_triton_output=True
-):
+def load_multi_llm_json_outputs(dir: str) -> Dict:
     result = {}
-    for language in languages:
-        result[language] = []
-        for idx in range(10000):
-            is_correct, input_path = get_file_path(
-                path, idx, use_triton=use_triton_input, input=True, language=language
-            )
-            _, output_path = get_file_path(
-                path, idx, use_triton=use_triton_output, input=False, language=language
-            )
-
-            if len(output_path) == 0 and len(input_path) == 0:
-                return result
-
-            result[language].append(
-                {
-                    "idx": idx,
-                    "marked_correct": 1 if is_correct else 0,
-                    "before_correction": read_input(input_path),
-                    "after__correction": read_output(output_path),
-                }
-            )
-
-        return result
-
-
-def load_files_to_dict_of_lists(
-    path, languages=["en"], use_triton_input=True, use_triton_output=True
-):
-    result = {}
-    for language in languages:
-        result[language] = {
-            "idx": [],
-            "marked_correct": [],
-            "before_correction": [],
-            "after_correction": [],
-        }
-
-        for idx in range(10000):
-            is_correct, input_path = get_file_path(
-                path, idx, use_triton=use_triton_input, input=True, language=language
-            )
-            _, output_path = get_file_path(
-                path, idx, use_triton=use_triton_output, input=False, language=language
-            )
-
-            if len(output_path) == 0 and len(input_path) == 0:
-                return result
-
-            result[language]["idx"].append(idx)
-            result[language]["marked_correct"].append(1 if is_correct else 0)
-            result[language]["before_correction"].append(read_input(input_path))
-            result[language]["after_correction"].append(read_output(output_path))
-
-    return result
-
-def load_multi_llm_json_outputs(dir: str, prompt_list: List[str]) -> Dict:
-    result = {}
-    pathlist = Path(dir).glob('**/*.json')
+    pathlist = Path(dir).glob("**/*.json")
     for path in pathlist:
         raw_filename = path.stem
         model_id = raw_filename.split("_")[0]
@@ -94,21 +16,24 @@ def load_multi_llm_json_outputs(dir: str, prompt_list: List[str]) -> Dict:
                 if lang not in result:
                     result[lang] = {}
                 for elem in data[lang]:
-                    id = elem['id']
+                    id = elem["id"]
                     if id not in result[lang]:
                         result[lang][id] = {
-                            "marked_correct": elem['label'],
-                            "text": elem['content'],
-                            "corrections": []
+                            "marked_correct": elem["label"],
+                            "text": elem["content"],
+                            "corrections": [],
                         }
-                    result[lang][id]['corrections'].append({
-                        "prompt_id": int(prompt_id),
-                        "content": elem['processed'],
-                        "model_name": model_id,
-                    })
-        with open("merged_multillm.json", 'w') as out:
+                    result[lang][id]["corrections"].append(
+                        {
+                            "prompt_id": int(prompt_id),
+                            "content": elem["processed"],
+                            "model_name": model_id,
+                        }
+                    )
+        with open("merged_multillm.json", "w") as out:
             out.write(json.dumps(result, indent=4, ensure_ascii=False))
     return result
+
 
 def read_input(path: str):
     if path.endswith("json"):
