5d108eefe1b803734fe0b78b0ff36e1657454fd0
Antoni Solarski <antoni.solarski@laniqo.com>
2024-08-07 10:55:56 +0200

    wip

--- run_llm.py
+++ run_llm.py
@@ -20,13 +20,10 @@ PROMPTS_SET = [
         "cs": "Upravte v následujícím textu pravopisné a gramatické chyby, vraťte pouze opravený text:"
     },
     {
-        "en": "Edit the following text for spelling and grammar mistakes, make minimal changes, and return only the corrected text. If the text is already correct, return it without any explanations:",
-        # FIXED , return
+        "en": "Edit the following text for spelling and grammar mistakes, make minimal changes, and return only the corrected text. If the text is already correct, return it without any explanations:", # FIXED , return
         "de": "Korrigieren Sie den folgenden Text auf Rechtschreib- und Grammatikfehler, nehmen Sie nur minimale Änderungen vor und senden Sie nur den korrigierten Text zurück. Wenn der Text bereits korrekt ist, senden Sie ihn ohne Erklärungen zurück:",
-        "it": "Modifica il testo seguente per errori di ortografia e grammatica, apporta modifiche minime e restituisci solo il testo corretto. Se il testo è già corretto, restituiscilo senza spiegazioni:",
-        # FIXED, bigger diff
-        "sv": "Redigera följande text för stavnings- och grammatikfel, gör minimala ändringar och returnera endast den korrigerade texten. Om texten redan är korrekt, returnera den utan några förklaringar:",
-        # FIXED , returnera
+        "it": "Modifica il testo seguente per errori di ortografia e grammatica, apporta modifiche minime e restituisci solo il testo corretto. Se il testo è già corretto, restituiscilo senza spiegazioni:", # FIXED, bigger diff
+        "sv": "Redigera följande text för stavnings- och grammatikfel, gör minimala ändringar och returnera endast den korrigerade texten. Om texten redan är korrekt, returnera den utan några förklaringar:", # FIXED , returnera
         "cs": "Upravte v následujícím textu pravopisné a gramatické chyby, proveďte minimální změny a vraťte pouze opravený text. Pokud je text již správný, vraťte jej bez vysvětlení:"
     }
 ]
@@ -36,7 +33,6 @@ MODELS = {
     "mistral": "mistralai/Mistral-7B-Instruct-v0.3",
     "llama31": "meta-llama/Meta-Llama-3.1-8B-Instruct",
     "openchat36": "openchat/openchat-3.6-8b-20240522",
-    "gpt_small": "microsoft/DialoGPT-small",
     "tower7B": "Unbabel/TowerInstruct-7B-v0.2",
     "tower13B": "Unbabel/TowerInstruct-13B-v0.1",
 }
@@ -90,57 +86,73 @@ def process_batch(prompt_idx, batch, language, model, tokenizer, device):
     return output
 
 
-def process_all(models, device):
+def process_all_prompts(data, device, model, model_id, tokenizer, iteration):
+    for prompt_idx in range(len(PROMPTS_SET)):
+        print("Processing prompt:", prompt_idx)
+
+        outputs = {}
+
+        for language in data:
+            idx = 0
+            batch_id = 0
+            if language not in outputs:
+                outputs[language] = []
+
+            for batch in batchify(data[language], only_texts=True):
+                print(f"{language}: processing batch: {batch_id}")
+                batch_id += 1
+
+                processed_texts = process_batch(prompt_idx, batch, language, model, tokenizer, device)
+                for text in processed_texts:
+                    e = data[language][idx]
+                    e['processed'] = text
+                    outputs[language].append(e)
+                    idx += 1
+
+        with open(f'{model_id}_output_prompt_{prompt_idx}_{iteration}.json', 'w') as f:
+            f.write(json.dumps(outputs, ensure_ascii=False, indent=4))
+
+
+def process_all_models(models, device, iterations):
+    with open('data_joined.json', 'r') as f:
+        data = json.load(f)
+    data = sort_data_lengths(data)
+
     for model_id, name in models.items():
+        if device == 'cuda':
+            torch.cuda.empty_cache()
         tokenizer = AutoTokenizer.from_pretrained(name)
         tokenizer.padding_side = 'left'
         tokenizer.pad_token = tokenizer.eos_token
         model = AutoModelForCausalLM.from_pretrained(name, torch_dtype=torch.float16, trust_remote_code=True).to(device)
 
-        for prompt_idx in range(len(PROMPTS_SET)):
-            with open('data_joined.json', 'r') as f:
-                data = json.load(f)
-            data = sort_data_lengths(data)
-            outputs = {}
-
-            for language in data:
-                idx = 0
-                batch_id = 0
-                if language not in outputs:
-                    outputs[language] = []
-
-                for batch in batchify(data[language], only_texts=True):
-                    print(f"{language}: processing batch: {batch_id}")
-                    batch_id += 1
-
-                    processed_texts = process_batch(prompt_idx, batch, language, model, tokenizer, device)
-                    for text in processed_texts:
-                        e = data[language][idx]
-                        e['processed'] = text
-                        outputs[language].append(e)
-                        idx += 1
-
-            with open(f'{model_id}_output_prompt_{prompt_idx}.json', 'w') as f:
-                f.write(json.dumps(outputs, ensure_ascii=False, indent=4))
+        print(f"Start processing model {model_id}: {name}")
+        for iteration in range(iterations):
+            print(f"Iteration: {iteration + 1}/{iterations}")
+            process_all_prompts(data, device, model, model_id, tokenizer, iteration)
+            print("-" * 50)
+        print(f"Done processing model {model_id}: {name}")
 
 
 def get_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser()
     parser.add_argument("--model", type=str, nargs="*")
     parser.add_argument("--device", type=str, default="cuda")
+    parser.add_argument("--iterations", type=int, default=1)
     return parser.parse_args()
 
 
 def main() -> None:
     args = get_args()
-
     models_to_process = MODELS if not args.model else {
         model_id: name for model_id, name in MODELS.items() if model_id in args.model
     }
 
-    device = args.device if args.device else 'cuda'
-
-    process_all(models_to_process, device)
+    process_all_models(
+        models=models_to_process,
+        device=args.device,
+        iterations=args.iterations,
+    )
 
 
 if __name__ == '__main__':
