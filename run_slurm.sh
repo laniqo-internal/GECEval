12947d748175048249aae04733cc5f6ff9537981
Antoni Solarski <antoni.solarski@laniqo.com>
2024-08-08 10:59:14 +0200

    final results for tower7B, results for gemma2B

--- run_slurm.sh
+++ run_slurm.sh
@@ -4,5 +4,5 @@ module load anaconda
 conda activate solarski
 cd /work/asolarski/GECEval
 #srun python3 run_llm.py --model tower7B --iterations 5
-srun python3 run_llm.py --model gemma2B --iterations 5
+srun python3 run_llm.py --model gemma2B --iterations 1
 
